cf55465f5cd5145578cf2edc02d6b3ce85f4651d
Isai <isaiorellana.dev@gmail.com>
2025-09-24 17:58:49 -0600

    hotfix: unused variable

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react"
 import logo from "../../../public/logo.png"
 import menu from "../../../public/menu.svg"
-import cart from "../../../public/shopping-cart.svg"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
